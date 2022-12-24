bd1f646b8a4b852adf6556b0916016bb6462ebef
Rakan Alotaibi <rakanalotaibi@Rakans-MacBook-Pro.local>
2022-12-25 00:38:39 +0300

    Added Tracking sheet 24/12

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tuwaiq_community/router/router.dart';
-import 'package:tuwaiq_community/views/MycCommunitiesPage/MycCommunitiesPage.dart';
-import 'package:tuwaiq_community/views/MycCommunitiesPage/components/TabBarTest.dart';
 import 'package:tuwaiq_community/views/ProfilePage/profilePage.dart';
 import 'package:tuwaiq_community/views/loginPage/LoginPage.dart';
 
@@ -17,7 +15,7 @@ class MyApp extends StatelessWidget {
       //  initialRoute: RouterName.loginPage,
       getPages: routerApp,
       locale: const Locale("ar", "SA"),
-      home: ProfilePage(),
+      home: LoginPage(),
     );
   }
 }
